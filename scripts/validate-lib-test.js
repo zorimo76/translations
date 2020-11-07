0642c02fa093469147c81a25bbf715a8a5ba90b9
Thomas Weber <weber_t@mailbox.org>
2020-11-06 18:44:30 -0600

    Refactoring validation and write a makeshift test suite

---
+++ scripts/validate-lib-test.js
@@ -0,0 +1,111 @@
+const {validateLanguage, validateMessage} = require('./validate-lib');
+
+const m = {
+  defaultMessage: '123',
+  description: 'abc',
+  message: null
+};
+
+const expectValidLanguage = (content) => {
+  const callback = (e) => {throw e;};
+  validateLanguage(content, callback);
+};
+
+const expectInvalidLanguage = (content) => {
+  let error = false;
+  const callback = (e) => error = true;
+  validateLanguage(content, callback);
+  if (!error) {
+    throw new Error('Expected invalid language');
+  }
+};
+
+const expectValidMessage = (key, message) => {
+  validateMessage(key, message);
+};
+
+const expectInvalidMessage = (key, message) => {
+  try {
+    validateMessage(key, message);
+    throw new Error('Expected invalid message');
+  } catch (e) {
+    // OK
+  }
+}
+
+expectValidLanguage(JSON.stringify({
+  'tw.123': m
+}));
+expectInvalidLanguage(JSON.stringify({
+  'tw.123': null
+}));
+expectInvalidLanguage('null');
+expectInvalidLanguage('123');
+expectInvalidLanguage('"123"');
+expectInvalidLanguage('[]');
+expectInvalidLanguage('{');
+
+expectValidMessage('tw.123', m);
+expectInvalidMessage('abc.123', m);
+expectInvalidMessage('tw.123', null);
+expectInvalidMessage('tw.123', "null");
+expectInvalidMessage('tw.123', 123);
+expectInvalidMessage('tw.123', []);
+expectInvalidMessage('tw.123', {
+  defaultMessage: '123',
+  description: 'abc',
+});
+expectInvalidMessage('tw.123', {
+  defaultMessage: '123',
+  message: null
+});
+expectInvalidMessage('tw.123', {
+  description: 'abc',
+  message: null
+});
+expectInvalidMessage('tw.123', {
+  ...m,
+  message: 123
+});
+expectInvalidMessage('tw.123', {
+  ...m,
+  description: null
+});
+expectInvalidMessage('tw.123', {
+  ...m,
+  defaultMessage: null
+});
+expectInvalidMessage('tw.123', {
+  ...m,
+  extra: true
+});
+expectValidMessage('tw.123', {
+  ...m,
+  defaultMessage: '{var}',
+  message: null
+});
+expectValidMessage('tw.123', {
+  ...m,
+  defaultMessage: 'foo {var}',
+  message: 'bar {var}'
+});
+expectInvalidMessage('tw.123', {
+  ...m,
+  defaultMessage: '{var}',
+  message: '{var2}'
+});
+expectInvalidMessage('tw.123', {
+  ...m,
+  defaultMessage: '{var}',
+  message: 'abc'
+});
+expectInvalidMessage('tw.123', {
+  ...m,
+  defaultMessage: '{var}',
+  message: '{var}{foo}'
+});
+expectInvalidMessage('tw.123', {
+  ...m,
+  defaultMessage: 'foo',
+  message: '{bar}'
+});
---
+++ scripts/validate-lib.js
@@ -0,0 +1,83 @@
+const isObject = (val) => typeof val === 'object' && val !== null && !Array.isArray(val);
+
+const validateLanguage = (content, errorCallback) => {
+  let parsedContent;
+  try {
+    parsedContent = JSON.parse(content);
+  } catch (e) {
+    errorCallback(new Error(`Invalid JSON: ${e.message}`));
+    return;
+  }
+
+  if (!isObject(parsedContent)) {
+    errorCallback(new Error(`Not an Object`));
+    return;
+  }
+
+  for (const key of Object.keys(parsedContent)) {
+    try {
+      const value = parsedContent[key];
+      validateMessage(key, value);
+    } catch (err) {
+      errorCallback(err);
+    }
+  }
+};
+
+const validateMessage = (key, value) => {
+  if (!key.startsWith('tw.')) {
+    throw new Error(`${key}: Does not start with tw.`);
+  }
+
+  if (!isObject(value)) {
+    throw new Error(`${key}: Not an object`);
+  }
+  const valueKeys = Object.keys(value);
+
+  if (!valueKeys.includes('message')) {
+    throw new Error(`${key}: Missing "message"`);
+  }
+  if (typeof value.message !== 'string' && value.message !== null) {
+    throw new Error(`${key}: Invalid "message" (not null or string)`);
+  }
+
+  if (!valueKeys.includes('description')) {
+    throw new Error(`${key}: Missing "description"`)
+  }
+  if (typeof value.description !== 'string') {
+    throw new Error(`${key}: Invalid "description"`);
+  }
+
+  if (!valueKeys.includes('defaultMessage')) {
+    throw new Error(`${key}: Missing "defaultMessage"`);
+  }
+  if (typeof value.defaultMessage !== 'string') {
+    throw new Error(`${key}: Invalid "defaultMessage"`);
+  }
+
+  if (valueKeys.length !== 3) {
+    throw new Error(`${key}: Has extra properties`);
+  }
+
+  if (value.message !== null) {
+    const defaultMessageVariables = value.defaultMessage.match(/{\w+}/g);
+    const defaultMessageVariableCount = defaultMessageVariables === null ? 0 : defaultMessageVariables.length;
+    const messageVariables = value.message.match(/{\w+}/g);
+    const messageVariablesCount = messageVariables === null ? 0 : messageVariables.length;
+
+    if (defaultMessageVariableCount !== messageVariablesCount) {
+      throw new Error(`${key}: Incorrect number of variables: expected ${defaultMessageVariableCount} but found ${messageVariablesCount}`);
+    }
+
+    for (let i = 0; i < messageVariablesCount; i++) {
+      if (defaultMessageVariables[i] !== messageVariables[i]) {
+        throw new Error(`${key}: Incorrect variables: expected ${defaultMessageVariables[i]} but found ${messageVariables[i]}`);
+      }
+    }
+  }
+};
+
+module.exports = {
+  validateLanguage,
+  validateMessage
+};
