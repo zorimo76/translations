41ad46fa35cd43e41b765f6c10c14c640c2caea0
Thomas Weber <weber_t@mailbox.org>
2020-11-07 18:43:01 -0600

    Add script to automatically pull from scratch-gui

---
+++ scripts/auto-load.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+start=$(pwd)
+
+cd ../scratch-gui
+npm run build
+
+cd $start
+mkdir -p in
+cp -r ../scratch-gui/translations/messages/src/ in
+
+node scripts/load-from-scratch-gui.js
