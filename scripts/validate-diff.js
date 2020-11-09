947ee077ccd3ca90f4bf73a32562720b9d3b049d
Thomas Weber <weber_t@mailbox.org>
2020-11-08 20:29:51 -0600

    Testing this more

--- scripts/validate-diff.js
+++ scripts/validate-diff.js
@@ -2,5 +2,29 @@ const fs = require('fs');
 
 const fileName = process.argv[2];
 const fileContent = fs.readFileSync(fileName, 'utf8');
+const lines = fileContent.split('\n');
+let valid = true;
+let currentFile = '?';
 
-console.log(fileContent);
+for (var i = 0; i < lines.length; i++) {
+  const line = lines[i].trim();
+
+  if (!line.startsWith('+') && !line.startsWith('-')) {
+    continue;
+  }
+  if (line.startsWith('---')) {
+    continue;
+  }
+  if (line.startsWith('+++')) {
+    currentFile = line.substr(6);
+    continue;
+  }
+  if (!line.includes('message:')) {
+    console.error(`${currentFile} unexpectedly changed something other than message:`);
+    valid = false;
+  }
+}
+
+if (!valid) {
+  process.exit(1);
+}
